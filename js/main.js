18a9c4826012a85c9f0c0593ffade25aa2dc5d1e
Nicolo <nicolorizzi1996@gmail.com>
2020-04-06 17:55:15 +0200

    ACQUISIZIONE NUMERI UTENTE

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-console.log('TEST OKAY');
 /**Descrizione******************************************
 * Il computer deve generare 16 numeri casuali tra 1 e 100 (numeri vietati).
 * In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
@@ -10,17 +9,39 @@ console.log('TEST OKAY');
  /******************************************************
   * PSEUDO CODICE:
   * GENERO 16 NUMERI CASUALI CONTENUTI NELL'INTERVALLO CHE VA DA 1 A 100
+  * CHIEDO ALL'UTENTE UN NUMERO DA INSERIRE
   * 
   ******************************************************/
 // ARRAY CONTENENTE I NUMERI VIETATI
 var numeriVietati = [];
-
+// ARRAY CONTENTENTE I NUMERI DICHIARATI DAL GIOCATORE
+var numeriUtente = [];
+// VARIABILE PER ASSEGNAMENTI DEI NUMERI CASUALI SIA ARRAY VIETATI CHE NUMERI UTENTE
+var numeroRandom = 0;
+// INIZIALIZZO LA GENERAZIONE DI UN CICLO PER 16 NUMERI (PER IL MOMENTO)
 for (var i = 1; i <= 16; i++) {
-  var numeroRandom = getRandomNumber(1, 100);
-  console.table("Il "+ i + "°" + " numero random generato è",numeroRandom);
+  numeroRandom = getRandomNumber(1, 100);
+  //console.table("Il "+ i + "°" + " numero random generato è",numeroRandom);
   numeriVietati.push(numeroRandom);
 }
-console.log(numeriVietati);
+// PRINT DELL'ARRAY VIETATI
+console.table(numeriVietati);
+//console.log("TERMINE DEL CICLO NUMERI RANDOM ARRAY", numeroRandom);
+
+// INIZIALIZZO IL CICLO PER UN NUMERO DI TENTATIVI MASSIMO DI 84 (PER IL MOMENTO)
+for (var tentativi = 1; tentativi <= 5; tentativi++){
+  var numeroRandom = parseInt(prompt("Inserisci il tuo numero da giocare compreso tra 1 e 100"));
+  if (numeroRandom < 0 || numeroRandom > 100) {
+    while (numeroRandom < 0 || numeroRandom > 100) {
+      var numeroRandom = parseInt(prompt("Attenzione hai inserito un numero che non è compreso!"));
+      console.log("QUI SEI NEL CICLO DI ERRORE",numeroRandom);
+      if (numeroRandom >= 0 || numeroRandom <= 100) {
+        numeriUtente.push(numeroRandom);
+   }  
+  }
+ }  numeriUtente.push(numeroRandom);
+}
+console.table("ARRAY DELL'UTENTE",numeriUtente);
 
 
 
